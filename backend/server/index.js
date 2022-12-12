d6dc631745e1281ca87e838a54b383cfc837f39c
John Binning <john.binning17@gmail.com>
2022-12-12 13:04:14 -0700

    Update index.js

--- backend/server/index.js
+++ backend/server/index.js
@@ -7,7 +7,7 @@ const finnhub = require('finnhub');
 const api_key = finnhub.ApiClient.instance.authentications['api_key'];
 api_key.apiKey = process.env.SECRET_KEY;
 
-const finnhubClient = new finnhub.DefaultApi()
+const finnhubClient = new finnhub.DefaultApi();
 
 app.get("/api/quote/:symbol", (req, res) => {
   finnhubClient.quote(req.params.symbol, (error, data, response) => {
