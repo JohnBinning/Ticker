40336eacd4d1fa458a89f19b0eb28e6f7b1e6315
John Binning <john.binning17@gmail.com>
2022-12-12 13:04:05 -0700

    add missing semicolons

--- backend/server/index.js
+++ backend/server/index.js
@@ -1,17 +1,17 @@
-require('dotenv').config()
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const PORT = 3030;
 
 const finnhub = require('finnhub');
 const api_key = finnhub.ApiClient.instance.authentications['api_key'];
-api_key.apiKey = process.env.SECRET_KEY
+api_key.apiKey = process.env.SECRET_KEY;
 
 const finnhubClient = new finnhub.DefaultApi()
 
 app.get("/api/quote/:symbol", (req, res) => {
   finnhubClient.quote(req.params.symbol, (error, data, response) => {
-    res.json(data)
+    res.json(data);
   });
 });
 
